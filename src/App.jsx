1747d3dfa8153fe8ff71c7b122f9ed227816250a
matias mendelberg <matiasmendelbergtabo@gmail.com>
2024-03-27 18:29:05 -0300

    primera entrega

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import Header from "./Header.jsx"
+
+function App() {
+  return (
+    <>
+      <Header></Header>
+      <main className='p-4 grow'>
+        <h1>home</h1>
+        <button>soy un boton</button>
+      </main>
+      <footer>
+        <p>&copy; Copyright 2024 - Matias Mendelberg</p>
+      </footer>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/Header.jsx
@@ -0,0 +1,15 @@
+
+
+function Header() {
+    return(
+        <header className="bg-slade-700">
+            <nav>
+                <a href="#">link1</a>
+                <a href="#">link2</a>
+                <img src="/menu-hamburguesa.png" alt="menu-hamburguesa" />
+            </nav>
+        </header>
+    )
+}
+
+export default Header
